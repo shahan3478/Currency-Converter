363eb9b20d65791500adc04e98cffef13c5b79a0
shahan-kp <shahan.ahmad@kalpayfinancials.com>
2025-12-22 13:40:58 +0500

    My initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+console.log('script.js working')
+const populate = async (value,currency) => {
+    let myStr = ""
+    url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_o0s8rP7b6cwT04Ud2R4tCnbZYphNv9KMy200JJR4&base_currency="+ currency 
+    let response = await fetch(url)
+    let rJson = await response.json()
+    document.querySelector(".output").style.display="block"
+
+
+    for (let key of Object.keys(rJson["data"])) {
+
+        myStr += `<tr>
+      <td>${key}</td>
+      <td>${rJson["data"][key]["code"]}</td>
+      <td>${rJson["data"][key]["value"] * value}</td>
+     </tr>`
+
+
+
+
+    }
+
+
+    const tableBody = document.querySelector('tbody');
+    tableBody.innerHTML = myStr;
+
+}
+const btn = document.querySelector('.btn');
+btn.addEventListener("click", (e) => {
+    e.preventDefault()
+    const value = parseInt(document.querySelector("input[name='quantity']").value);
+    const currency = document.querySelector("select[name='Currency']").value;
+    populate(value, currency)
+})
